df0e4bcdd13a242ff199eaa20151bc13c211d189
Ramon Rainer <ramon.rainer@ongoing.ch>
2023-03-31 16:32:55 +0200

    feat: implement password reset

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
--- src/MessageHandler/ResetPasswordRequestMessageHandler.php
+++ src/MessageHandler/ResetPasswordRequestMessageHandler.php
@@ -2,14 +2,57 @@
 
 namespace App\MessageHandler;
 
+use App\Entity\User;
 use App\Message\ResetPasswordRequestMessage;
-use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Routing\RouterInterface;
+use SymfonyCasts\Bundle\ResetPassword\ResetPasswordHelperInterface;
 
-final class ResetPasswordRequestMessageHandler implements MessageHandlerInterface
+#[AsMessageHandler]
+final class ResetPasswordRequestMessageHandler
 {
+
+    public function __construct(
+        private ResetPasswordHelperInterface $resetPasswordHelper,
+        private EntityManagerInterface $entityManager,
+        private MailerInterface $mailer,
+        private RouterInterface $router,
+        private string $hostname,
+        private string $emailFrom,
+    ) {
+    }
+
     public function __invoke(ResetPasswordRequestMessage $message)
     {
-        //dump($message);
-        // do something with your message
+        $email = $message->getEmail();
+
+        $user = $this->entityManager->getRepository(User::class)->findOneBy([
+            'email' => $email,
+        ]);
+
+        // Do not reveal whether a user account was found or not.
+        if (!$user) {
+            throw new \Exception('User not found');
+        }
+
+        $resetToken = $this->resetPasswordHelper->generateResetToken($user);
+
+        $this->router->getContext()->setHost($this->hostname)->setScheme('https');
+
+        $email = (new TemplatedEmail())
+            ->from(new Address($this->emailFrom, 'Shop Demo'))
+            ->to($user->getEmail())
+            ->subject('Your password reset request')
+            ->htmlTemplate('reset_password/email.html.twig')
+            ->context([
+                'resetToken' => $resetToken,
+            ])
+        ;
+
+        $this->mailer->send($email);
     }
 }
